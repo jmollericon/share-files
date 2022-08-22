f0f4b27e20ba14b056f1a6cbd14e2da70361c8cd
Jorge Mollericon <jlmollericon@gmail.com>
2022-08-22 15:32:03 -0400

    🎨 add a button to do the test

--- src/views/BandwidthSpeed/BandwidthSpeed.tsx
+++ src/views/BandwidthSpeed/BandwidthSpeed.tsx
@@ -1,12 +1,38 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+
+const getNetworkBandwidth = async (): Promise<void> => {
+  console.log('getNetworkBandwidth');
+  try {
+    console.log('here');
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const BandwidthSpeed = () => {
   return (
     <View>
       <Text>BandwidthSpeed</Text>
+      <TouchableOpacity
+        onPress={() => getNetworkBandwidth()}
+        style={styles.button}>
+        <Text style={styles.text}>AQUIIII</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default BandwidthSpeed;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#123456',
+    padding: 10,
+  },
+  text: {
+    color: 'white',
+    fontSize: 25,
+    textAlign: 'center',
+  },
+});
