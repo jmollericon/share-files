12e8750ebb0430e3bcfe63c5317fcd4e2f1ddf87
Jorge Mollericon <jlmollericon@gmail.com>
2024-02-01 12:32:04 -0400

    🚧 Wip

--- src/views/BandwidthSpeed/BandwidthSpeed.tsx
+++ src/views/BandwidthSpeed/BandwidthSpeed.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 import RNFetchBlob from 'rn-fetch-blob';
 
 const downloadSizeInBits = 21135528; // bits
-const metric = 'MBps';
+const metric = 'Mbps';
 
 const measureConnectionSpeed = (imageURIParam: any): any => {
   const imageURI = imageURIParam
@@ -14,13 +14,14 @@ const measureConnectionSpeed = (imageURIParam: any): any => {
   // 'https://drive.google.com/open?id=1MBHJXeRxMLLwHFpqbgTdEPsFArMM0cz7'; // https://www.google.com
 
   const startTime = new Date().getTime();
-
+  console.log('before');
   return new Promise((resolve, reject) => {
     RNFetchBlob.config({
       fileCache: false,
     })
       .fetch('GET', imageURI, {})
       .then(() => {
+        console.log('completed');
         const endTime = new Date().getTime();
         const duration = (endTime - startTime) / 1000; // sec
         const speed = downloadSizeInBits / (duration * 1000000);
@@ -31,13 +32,14 @@ const measureConnectionSpeed = (imageURIParam: any): any => {
   });
 };
 
-const getNetworkBandwidth = async (): Promise<void> => {
+const getNetworkBandwidth = async (): Promise<any> => {
   console.log('getNetworkBandwidth');
   try {
     const speeds = [];
-    const counter = 5;
+    const counter = 2;
 
     for (let i = 0; i < counter; i++) {
+      console.log('i = ', i);
       const networkSpeed = await measureConnectionSpeed(null);
       speeds.push(networkSpeed.speed);
       console.log({networkSpeed}); // Network bandwidth speed, Mbps
@@ -45,21 +47,34 @@ const getNetworkBandwidth = async (): Promise<void> => {
 
     const sum = speeds.reduce((a, b) => a + b);
     const average_speed = sum / counter;
-    console.log('average_speed', average_speed, 'Mbps');
+
+    return average_speed.toFixed(2);
   } catch (err) {
     console.log(err);
   }
 };
 
 const BandwidthSpeed = () => {
+  const [speed, setSpeed] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const onPress = async () => {
+    setLoading(true);
+    setSpeed(0);
+    const speed_avg = await getNetworkBandwidth();
+    console.log('average_speed', speed_avg, 'Mbps');
+    setSpeed(speed_avg);
+    setLoading(false);
+  };
+
   return (
     <View>
       <Text>BandwidthSpeed</Text>
-      <TouchableOpacity
-        onPress={() => getNetworkBandwidth()}
-        style={styles.button}>
+      <TouchableOpacity onPress={() => onPress()} style={styles.button}>
         <Text style={styles.text}>AQUIIII</Text>
       </TouchableOpacity>
+      <Text style={styles.speed}>{speed} Mbps</Text>
+      {loading && <Text style={styles.speed}>Procesando...</Text>}
     </View>
   );
 };
@@ -73,6 +88,10 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'white',
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  speed: {
     fontSize: 25,
     textAlign: 'center',
   },
